a1cf73d39d86c0ab1a09d993259cac2eefad58a8
lynn <devlynn@126.com>
2018-12-26 15:36:20 +0800

    add note comment

--- source/header.swift
+++ source/header.swift
@@ -1,4 +1,4 @@
-
+/*
 Design Patterns implemented in Swift 3.0
 ========================================
 A short cheat-sheet with Xcode 8.2 Playground ([Design-Patterns.playground.zip](https://raw.githubusercontent.com/ochococo/Design-Patterns-In-Swift/master/Design-Patterns.playground.zip)).
@@ -14,3 +14,29 @@ A short cheat-sheet with Xcode 8.2 Playground ([Design-Patterns.playground.zip](
 * [Structural](#structural)
 
 */
+
+
+/*
+ 模式: 在某情境下, 针对某类特定问题的某种解决方案.
+ 
+ 创建型:
+ * 工厂模式: 由子类决定要创建的具体类是哪一个.
+ * 抽象工厂: 允许客户创建对象的家族, 而无需指定它们的具体类.
+ * 单例模式: 确保有且只有一个对象被创建.
+
+ 结构型:
+ * 装饰器: 包装一个对象, 以提供新的行为.
+ * 外观模式: 简化一群类的接口.
+ * 适配器: 封装对象, 并提供不同的接口.
+ * 代理模式: 包装对象, 以控制对该对象的访问.
+ * 组合模式: 客户用一致的方式处理对象集合和单个对象.
+
+ 行为型:
+ * 模板模式: 提供算法模板, 由子类决定如何实现一个算法中的某些步骤.
+ * 策略模式: 封装可以互换的行为, 并使用委托来决定要使用哪一个.
+ * 状态模式: 封装了基于状态的行为, 并使用委托在行为之间切换.
+ * 迭代器: 在对象的集合之中游走, 而不是暴露集合的实现.
+ * 观察者模式: 让对象能够在状态改变时被通知.
+ * 命令模式: 封装请求成为对象.
+ 
+ */
